e7d29c75b0a98b113cfe509bbffb108b6b3fa9cf
juan <juanjose1092@gmail.com>
2022-03-27 13:46:21 -0500

    added nsg rule

--- virtual-networks/vnlab/vnlab.tf
+++ virtual-networks/vnlab/vnlab.tf
@@ -21,6 +21,18 @@ resource "azurerm_network_security_group" "nsglab" {
   name                = "nsblab"
   location            = "North Central US"
   resource_group_name = "rglab"
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "vnetlab" {
