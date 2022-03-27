75017ece039ceebdd9442efd10bba303b6796d7f
juan <juanjose1092@gmail.com>
2022-03-27 16:26:09 -0500

    redeployed networking

--- virtual-networks/vnlab/vnlab.tf
+++ virtual-networks/vnlab/vnlab.tf
@@ -1,25 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0.2"
-    }
-  }
-    backend "azurerm" {
-        resource_group_name  = "rglab"
-        storage_account_name = "stlabylt3i"
-        container_name       = "tfstate"
-        key                  = "virtual-networks/vnlab/terraform.tfstate"
-    }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_network_security_group" "nsglab" {
-  name                = "nsblab"
-  location            = "North Central US"
+  name                = "nsglab"
+  location            = "Central US"
   resource_group_name = "rglab"
 
   security_rule {
@@ -37,12 +18,12 @@ resource "azurerm_network_security_group" "nsglab" {
 
 resource "azurerm_virtual_network" "vnetlab" {
   name                = "vnetlab"
-  location            = "North Central US"
+  location            = "Central US"
   resource_group_name = "rglab"
   address_space       = ["10.0.0.0/16"]
 
   subnet {
-    name           = "subnet1"
+    name           = "labsubnet"
     address_prefix = "10.0.1.0/24"
   }
 
